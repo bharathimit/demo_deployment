49b7989e7253e4017ab4cab94475ba24bd1db1a5
Chidambara Bharathi <cbharathi@anpi.com>
2015-07-13 21:35:19 +0000

    Unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,13 @@
-root = '/apps/tmp/demo_deployment'
+environment = ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'staging'
+
+if environment == 'staging'
+  root = '/apps/dd/current'
+  listen '10.5.3.203:8001'
+else
+  root = '/apps/tmp/demo_deployment'
+  listen '10.5.3.204:9001'
+end
+
 working_directory root
 
 pid "#{root}/tmp/pids/unicorn.pid"
@@ -8,7 +17,6 @@ stdout_path "#{root}/log/unicorn.log"
 worker_processes 4
 timeout 180
 
-listen '10.5.3.204:9001'
 
 
 preload_app true
